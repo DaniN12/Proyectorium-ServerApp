6a489060a53c0c19451c4fdfdc832e49b5279397
Dani <2dam@HZ375760.TartangaLH.eus>
2024-12-17 09:06:20 +0100

    Category entity arreglada

--- src/java/proyectorium/crud/entities/CategoryEntity.java
+++ src/java/proyectorium/crud/entities/CategoryEntity.java
@@ -7,13 +7,18 @@ package proyectorium.crud.entities;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -24,29 +29,34 @@ import javax.xml.bind.annotation.XmlRootElement;
  * @author Ruth
  */
 @Entity
-@Table(schema="proyectorium", name="Category")
+@Table(schema = "proyectorium", name = "Category")
 @XmlRootElement
 public class CategoryEntity implements Serializable {
+
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-     Integer id;
-    
+    Integer id;
+
     @Lob
     private byte[] icon;
     private String name;
     private String description;
-    
+
     @Temporal(TemporalType.TIMESTAMP)
     private Date creationDate;
-    
+
     @Enumerated(EnumType.STRING)
     private Pegi pegi;
 
-    public CategoryEntity(){
-        
+    @ManyToMany(mappedBy = "categories")
+    private List<MovieEntity> movies;
+
+    public CategoryEntity() {
+
     }
+
     public Integer getId() {
         return id;
     }
@@ -93,7 +103,15 @@ public class CategoryEntity implements Serializable {
 
     public void setCreationDate(Date creationDate) {
         this.creationDate = creationDate;
-    }    
+    }
+
+    public List<MovieEntity> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<MovieEntity> movies) {
+        this.movies = movies;
+    }
 
     @Override
     public int hashCode() {
@@ -119,5 +137,5 @@ public class CategoryEntity implements Serializable {
     public String toString() {
         return "proyectorium.crud.entities.CategoryEntity[ id=" + id + " ]";
     }
-    
+
 }
